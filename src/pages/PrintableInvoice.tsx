3085acebbe86ad0c9afffb200d556dba6af5aac1
[dyad] <git@dyad.sh>
2025-07-21 22:58:57 +0200

    [dyad] wrote 1 file(s)

--- src/pages/PrintableInvoice.tsx
+++ src/pages/PrintableInvoice.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } => 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSession } from '@/contexts/SessionContext';
 import { toast } from 'sonner';
